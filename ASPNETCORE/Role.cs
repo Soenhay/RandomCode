26b463e19ba99832afeedfacdd4f4e43144e0aca
Soenhay <exphosed@yahoo.com>
2019-12-17 13:01:34 -0600

    Create Role.cs

---
+++ ASPNETCORE/Role.cs
@@ -0,0 +1,12 @@
+namespace WebApi.Authorization
+{
+    /// <summary>
+    /// These roles should reflect what is in the database.
+    /// </summary>
+    public static class Role
+    {
+        public const string SuperUser = "SuperUser";
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
