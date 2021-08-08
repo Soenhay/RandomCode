bf2be48f82c485c3f0eb48f9bebb7cfe8aca8d54
Soenhay <exphosed@yahoo.com>
2021-08-08 12:58:56 -0500

    update HumbleBundleWebScraper.js to show claimed and csv.

--- Javascript/HumbleBundleWebScraper/HumbleBundleWebScraper.js
+++ Javascript/HumbleBundleWebScraper/HumbleBundleWebScraper.js
@@ -1,29 +1,39 @@
 /*
 Created by Soenhay
 
-This code can be used to output all of your unclaimed keys to the console. 
-Just click "Hide Redeemed Keys" and run it for each page of keys.
-It also works on the humble choice pages. 
-Once you copy all of the output into a text file you can then sort it with an online tool.
+This code can be used to output information about your humble bundle games to the console. 
+
+For the old way, go to "Keys and Entitlements". Then run the script on each page.
+For the new way, go to the "Make my Choices" page for each month. Then run the script on each page.
 
 03/06/2019 First version.
 04/11/2020 Updated for Humble Choice.
+8/8/2021 Updated to show monthlyTitle, claimed, gameName in csv format.
 
 */
 
-var gameNames = '';
-$('.unredeemed-keys-table').find('.game-name').each(function(){
-var monthlyTitle = $(this).find('p').attr('title'); 
-var gameName = $(this).find('h4').attr('title'); 
-  if(gameName){
-  gameNames+= monthlyTitle + ': ' + gameName.trim() + '\n';
-  }
- });
-$('.content-choice').not('.claimed').find('.choice-content > .title-and-delivery-methods > .content-choice-title').each(function(){
-var monthlyTitle = $('.content-choices-title > span').text().trim();
-var gameName = $(this).text();
-    if(gameName){
-  gameNames+= monthlyTitle + ': ' + gameName.trim() + '\n';
+//Keys and Entitlements. Pre humble choice.
+if ($('.js-key-manager-holder').find('div:contains("Keys & Entitlements")').length > 0) {
+  var gameNames = '';
+  $('.unredeemed-keys-table').find('.game-name').each(function () {
+    var monthlyTitle = $(this).find('p').attr('title');
+    if(monthlyTitle && !monthlyTitle.includes('Humble Choice'))
+    {
+      var claimed = ($(this).siblings().find('.container > .redeemed').length > 0);
+      var gameName = $(this).find('h4').attr('title');
+      gameNames += `${monthlyTitle}, ${claimed}, ${gameName}\n`;
     }
-});
-console.log(gameNames);
+  });
+  console.log(gameNames);
+}
+else {
+  //Humble choice.
+  var gameNames = '';
+  $('.content-choice').each(function () {
+    var monthlyTitle = $('.content-choices-title > span').text().trim();
+    var claimed = $(this).hasClass('claimed');
+    var gameName = $(this).find('.choice-content > .title-and-delivery-methods > .content-choice-title').text().trim();
+    gameNames += `${monthlyTitle}, ${claimed}, ${gameName}\n`;
+  });
+  console.log(gameNames);
+}
